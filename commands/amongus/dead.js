fad17aabb00eef386229f6be9cfb01dc759229d1
recon <66986299+reconlx@users.noreply.github.com>
2020-10-15 17:39:12 +0800

    Update dead.js

--- commands/amongus/dead.js
+++ commands/amongus/dead.js
@@ -1,18 +1,16 @@
 const { Message } = require("discord.js");
 const { prefix } = require('../../config/config.json')
-const dbs = require('../../db')
 const { MessageEmbed } = require('discord.js')
 module.exports = {
-  name: "au-dead",
+  name: "dead",
   description : 'Marks a player as **dead** and mute them from the discord call.',
   /**
    * @param {Message} message
    */
   run: async (client, message, args) => {
-    const p = await dbs.get(`Prefix_${message.guild.id}`) ? await dbs.get(`Prefix_${message.guild.id}`) : prefix
     const role = message.guild.roles.cache.find((r) => r.name === 'amongus-moderator')
     //--------------------------------------------------------------------------------------------------------
-    if (!role) return message.channel.send(`Among Us has not been setup, please run \`${p}au-setup\` to set it up.`)
+    if (!role) return message.channel.send(`Among Us has not been setup, please run \`${prefix}setup\` to set it up.`)
     if (!message.member.roles.cache.has(role.id)) return message.channel.send(new MessageEmbed()
         .setDescription(`This command can only be used by members that has ${role}, role.`).setColor('RED')
     )
